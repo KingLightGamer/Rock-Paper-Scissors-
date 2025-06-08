600cf11da8f4e0443e97dd88f7d5d6cc65cedaa2
KingLightGamer <208028170+KingLightGamer@users.noreply.github.com>
2025-06-08 19:56:04 +0200

    comlete change of the code because it didnt made any sence

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 function getComputerChoice() {
-    const choice= ["Rock","Paper","Scissor"];
-    const index = Math.floor(Math.random() * choice.length);
-    return choice[index];
+  let randomNumber = Math.random();
+  if (randomNumber < 1 / 3) {
+    return "rock";
+  } else if (randomNumber < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
-console.log (getComputerChoice());
-
-
+console.log(getComputerChoice);
 function getHumanChoice() {
-   const signButton = document.querySelector("#signButton");
-const log = document.querySelector("#log");
+  let input = prompt("Choose rock, paper, or scissors:");
+  return input.toLowerCase(); // case-insensitive
+}
+console.log(getHumanChoice);
 
-signButton.addEventListener("click", () => {
-  let sign = prompt("Rock,Paper or Scissor?");
-
-  if (sign === null) {
-    log.innerText = "OK, maybe next time.";
-  } else if (sign.toLowerCase() === "") {
-    log.innerText = "Don't be shy, choose!";
-  } else if (sign === "Rock") {
-    log.innerText = "You chose Rock";}
-    else if (sign === "Paper") {
-    log.innerText = "You chose Paper";}
-    else if (sign === "Scissor") {
-    log.innerText = "You chose Scissor";}
-    else {
-    log.innerText = `You need to choose Rock,Paper or Scissor!`;}
-});}
-console.log(getHumanChoice());
-let humanScore =0;
-let computerScore =0;
+let humanScore = 0;
+let computerScore = 0;
